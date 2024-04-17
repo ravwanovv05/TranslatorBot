4d4d6e079f26ceaeb902531e30c02b8656ed435d
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-04-17 15:53:38 +0500

    Added Webhook

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import logging
 from aiogram import types
 from aiogram.types import InlineKeyboardButton, WebAppInfo, InlineKeyboardMarkup
 from aiogram.utils import executor
+from aiogram.utils.executor import start_webhook
+
 from bot.dispatcher import dp, bot
 from bot.handlers.main_handlers import start_handler, help_handler
 from bot.handlers.set_language_handlers import language_list_handler, next_back_language_handler, set_language_handler, \
     my_selected_language_handler, delete_language_handler
 from bot.handlers.translate_message_handlers import translate_message_handler
+from bot.handlers.main_handlers import WEBHOOK_URL_PATH, on_startup, on_shutdown, WEBAPP_HOST, WEBAPP_PORT
+
 
 dp.register_message_handler(start_handler, commands=['start'])
 dp.register_message_handler(help_handler, lambda message: message.text == 'Yordam 🆘')
 
 
-@dp.message_handler(commands='instagram')
+@dp.message_handler(commands=['instagram'])
 async def start_handler(message: types.Message):
 
     design = [
@@ -33,4 +37,12 @@ dp.register_message_handler(translate_message_handler)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_URL_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
